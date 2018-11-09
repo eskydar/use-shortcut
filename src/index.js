c2f3264e8058aa8c668fb0f6e7a363f83205eaf7
Sebastiaan van Arkens <sebastiaan@vanarkens.nl>
2018-11-10 00:29:49 +0100

    Create use-shortcut

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const useShortcuts = () => {
+  const [newActiveKeys, setNewActiveKeys] = useState([]);
+
+  const handleKeydown = (e) => {
+    if (newActiveKeys.indexOf(e.key) > -1) return;
+    setNewActiveKeys([...newActiveKeys, e.key.toLowerCase()]);
+  };
+
+  const handleKeyup = (e) => setNewActiveKeys([...newActiveKeys.filter((key) => key !== e.key.toLowerCase())]);
+
+  const isShortcut = (shortcut) => shortcut.split('+').every((key) => newActiveKeys.indexOf(key.toLowerCase()) > -1);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeydown);
+    window.addEventListener('keyup', handleKeyup);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+      window.removeEventListener('keyup', handleKeyup);
+    };
+  });
+
+  return [newActiveKeys, isShortcut];
+};
+
+export default useShortcuts;
