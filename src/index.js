31898f223a0ce977ebf50534ef85417854f49216
Sebastiaan van Arkens <sebastiaan@vanarkens.nl>
2018-11-12 13:14:03 +0100

    added keycodes object to prevent modifier key to apply on key

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react';
 
+import keycodes from './keycodes';
+
 const useShortcut = () => {
   const [newActiveKeys, setNewActiveKeys] = useState([]);
 
   const handleKeydown = (e) => {
-    if (newActiveKeys.indexOf(e.key) > -1) return;
-    setNewActiveKeys([...newActiveKeys, e.key.toLowerCase()]);
+    if (newActiveKeys.indexOf(keycodes[e.keyCode]) > -1) return;
+    setNewActiveKeys([...newActiveKeys, keycodes[e.keyCode]]);
   };
 
-  const handleKeyup = (e) => setNewActiveKeys([...newActiveKeys.filter((key) => key !== e.key.toLowerCase())]);
+  const handleKeyup = (e) => setNewActiveKeys([...newActiveKeys.filter((key) => key !== keycodes[e.keyCode])]);
 
-  const isShortcut = (shortcut) => shortcut.split('+').every((key) => newActiveKeys.indexOf(key.toLowerCase()) > -1);
+  const isShortcut = (shortcut) => shortcut.split('+').every((key) => newActiveKeys.indexOf(key) > -1);
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeydown);
